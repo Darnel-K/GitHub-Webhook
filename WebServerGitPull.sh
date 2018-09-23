67c6b5fbdb3e9159babace599dbb470f36528d36
Darnel Kumar <darnel.kumar91@gmail.com>
2018-09-23 19:23:13 +0100

    Inital Commit

---
+++ WebServerGitPull.sh
@@ -0,0 +1,34 @@
+#!/usr/local/bin/bash
+set -e
+set -o pipefail
+
+if [[ $# -eq 0 ]] ; then
+    echo "Repository Not Specified, Script Terminated!"
+    exit 1
+fi
+
+if [ -z "$1" ]
+  then
+    echo "Repository Not Specified, Script Terminated!"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "File Path Not Specified, Script Terminated!"
+    exit 1
+fi
+
+GitHubRepoDirectory=$2
+Repository=$1
+
+printf "\nRepository Name: ${Repository}"
+printf "\nRepository Full Path: ${GitHubRepoDirectory}\n\n"
+
+cd "${GitHubRepoDirectory}"
+git reset --hard
+git pull --verbose
+
+cd "${GitHubRepoDirectory}"
+printf "\n#### Setting Permissions ####\n\n"
+chmod -Rv ug=rwx,o=rx .
---
+++ pull.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'config.php'; // Edit this file to add repos
+
+$headers = getallheaders();
+list($algo, $hash) = explode('=', $headers['X-Hub-Signature'], 2);
+$payload = file_get_contents('php://input');
+$payloadHash = hash_hmac($algo, $payload, SECRET);
+if ($hash !== $payloadHash) {
+    exit('Unable To Validate Request, Script Terminated!');
+}
+$payload = json_decode($payload, true);
+
+$RepoName = $payload['repository']['name'];
+$RepoFullName = $payload['repository']['full_name'];
+$ref = $payload['ref'];
+$private = $payload['repository']['private'];
+
+if (!$private) {
+    echo "Private: False";
+} else {
+    echo "Private: True, Credentials Will Be Required!";
+}
+
+if (isset($Repositories[$RepoName][$ref])) {
+    echo shell_exec("./WebServerGitPull.sh '" . $RepoName . "' '" . $Repositories[$RepoName][$ref] . "' 2>&1");
+} else if (isset($Repositories[$RepoFullName][$ref])) {
+    echo shell_exec("./WebServerGitPull.sh '" . $RepoFullName . "' '" . $Repositories[$RepoFullName][$ref] . "' 2>&1");
+} else {
+    exit("Repository Not Specified, Script Terminated!");
+}
+
+?>

6e8ad18e1fdd9236a8206126bfec3612611f7310
Darnel Kumar <darnel.kumar91@gmail.com>
2018-10-23 14:38:30 +0100

    read settings from config.json

--- Main.py
+++ Main.py
@@ -3,14 +3,19 @@ import cgi
 import json
 import cgitb
 import sys
-
+import hmac
 import os
+
+f = open('config.json', 'r')
+Settings = json.loads(f.read())
+
+Payload = sys.stdin.read(int(os.environ["CONTENT_LENGTH"]))
 Data = {
     "X-GitHub-Delivery": os.environ["HTTP_X_GITHUB_DELIVERY"],
     "X-GitHub-Event": os.environ["HTTP_X_GITHUB_EVENT"],
     "X-Hub-Signature": os.environ["HTTP_X_HUB_SIGNATURE"],
     "User-Agent": os.environ["HTTP_USER_AGENT"],
-    "Payload": json.loads(sys.stdin.read(int(os.environ["CONTENT_LENGTH"])))
+    "Payload": json.loads(Payload)
 }
 
 print("Content-Type: application/json; charset=utf-8\n\n")

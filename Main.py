c09969ebaa35b5d2ef1b6547095b73b8b9b5896b
Darnel Kumar <darnel.kumar91@gmail.com>
2018-10-23 14:20:51 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ Data = {
     "X-GitHub-Event": os.environ["HTTP_X_GITHUB_EVENT"],
     "X-Hub-Signature": os.environ["HTTP_X_HUB_SIGNATURE"],
     "User-Agent": os.environ["HTTP_USER_AGENT"],
-    "Payload": sys.stdin.read(int(os.environ["CONTENT_LENGTH"]))
+    "Payload": json.loads(sys.stdin.read(int(os.environ["CONTENT_LENGTH"])))
 }
 
 print("Content-Type: application/json; charset=utf-8\n\n")

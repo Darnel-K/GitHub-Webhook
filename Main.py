7d5026b9f8fd89d52e150c9e3afbdc8b12d53e98
Darnel Kumar <darnel.kumar91@gmail.com>
2018-10-23 13:25:05 +0100

    test

--- Main.py
+++ Main.py
@@ -1,2 +1,14 @@
+#!/usr/local/bin/python3.6
+# import cgi
+# import json
+# import cgitb
 
+import os
 
+print("Content-Type: text/html")
+print("Cache-Control: no-cache")
+print()
+
+print("<html><body><code>")
+print(os.environ)
+print("</code></html></body>")

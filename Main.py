40117b0f761211a9ef466c45cf545388ae1f72de
Darnel Kumar <darnel.kumar91@gmail.com>
2018-10-23 14:18:05 +0100

    Changed fieldstorage to read from standard input

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 import cgi
 import json
 import cgitb
+import sys
 
 import os
 Data = {
@@ -9,10 +10,9 @@ Data = {
     "X-GitHub-Event": os.environ["HTTP_X_GITHUB_EVENT"],
     "X-Hub-Signature": os.environ["HTTP_X_HUB_SIGNATURE"],
     "User-Agent": os.environ["HTTP_USER_AGENT"],
-    "Payload": cgi.FieldStorage()
+    "Payload": sys.stdin.read(int(os.environ["CONTENT_LENGTH"]))
 }
 
-print("Content-Type: application/json")
-print()
+print("Content-Type: application/json; charset=utf-8\n\n")
 
 print(json.dumps(Data))

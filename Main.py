ec2a3429e752cc3e88269a38024bcfb828950a03
Darnel Kumar <darnel.kumar91@gmail.com>
2018-10-30 14:28:45 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,6 +8,18 @@ import os
 import subprocess
 
 
+class cd:
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+
 def exit_on_error(e, MSG, eCode):
     global DataStorage
     DataStorage["Error"].append({"e": e, "msg": MSG, "error_code": eCode})
@@ -33,75 +45,53 @@ DataStorage = {
     "Payload": json.loads(RawPayload),
     "OutputType": Settings['OutputType'],
     "Charset": Settings["Charset"],
-    "Git": {
-        "Status": {
-            "Before": None,
-            "After": None
-        }
-    }
+    "Branch": DataStorage['Payload']['ref'].rsplit('/', 1)[-1],
 }
 
 
 def process_output(Data):
     output = {
-        "Error": Data['Error'],
-        "Git": Data['Git'],
-        "X-GitHub-Delivery": Data['X-GitHub-Delivery'],
-        "X-GitHub-Event": Data['X-GitHub-Event'],
-        "X-Hub-Signature": Data['X-Hub-Signature'],
-        "User-Agent": Data['User-Agent'],
-        "User": {
-            "Name": Data['Payload']['sender']['login'],
-            "Photo": Data['Payload']['sender']['avatar_url'],
-            "URL": Data['Payload']['sender']['html_url'],
-            "ID": Data['Payload']['sender']['id']
-        },
-        "Pusher": {
-            "Name": Data['Payload']['pusher']['name'],
-            "Email": Data['Payload']['pusher']['email']
-        },
-        "Repository": {
-            "ID": Data['Payload']['repository']['id'],
-            "Name": Data['Payload']['repository']['name'],
-            "FullName": Data['Payload']['repository']['full_name'],
-            "Private": Data['Payload']['repository']['private'],
-            "URL": Data['Payload']['repository']['html_url'],
-            "CloneURL": Data['Payload']['repository']['clone_url'],
-            "GitURL": Data['Payload']['repository']['git_url'],
-            "SSH_URL": Data['Payload']['repository']['ssh_url'],
-            "SVN_URL": Data['Payload']['repository']['svn_url'],
-            "Homepage": Data['Payload']['repository']['homepage'],
-            "Description": Data['Payload']['repository']['description'],
-            "Owner": {
-                "Name": Data['Payload']['repository']['owner']['name'],
-                "Email": Data['Payload']['repository']['owner']['email'],
-                "Login": Data['Payload']['repository']['owner']['login'],
-                "ID": Data['Payload']['repository']['owner']['id'],
-                "Photo": Data['Payload']['repository']['owner']['avatar_url'],
-                "URL": Data['Payload']['repository']['owner']['html_url']
-            }
-        },
-        "ref": Data['Payload']['ref'],
-        "LatestCommitID": Data['Payload']['after'],
-        "Branch": Data['Payload']['ref'].rsplit('/', 1)[-1],
-        "Compare": Data['Payload']['compare'],
-        "Commits": Data['Payload']['commits']
+        "Error": DataStorage['Error'],
+        "X-GitHub-Delivery": DataStorage['X-GitHub-Delivery'],
+        "X-GitHub-Event": DataStorage['X-GitHub-Event'],
+        "X-Hub-Signature": DataStorage['X-Hub-Signature'],
+        "User-Agent": DataStorage['User-Agent'],
+        "ref": DataStorage['Payload']['ref'],
+        "LatestCommitID": DataStorage['Payload']['after'],
+        "Branch": DataStorage['Branch'],
+        "Compare": DataStorage['Payload']['compare'],
+        "Commits": DataStorage['Payload']['commits'],
+        "Commands": Data
     }
     return output
 
 
-def git_status():
-    out = {
-        "STDOUT": [],
-        "STDERR": []
-    }
-    s = subprocess.run('git status -vv --long', stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE, shell=True)
-    for i in s.stdout.decode('utf-8').splitlines():
-        out['STDOUT'].append(str(i))
-    for i in s.stderr.decode('utf-8').splitlines():
-        out['STDERR'].append(str(i))
-    return out
+# def git_status():
+#     out = {
+#         "STDOUT": [],
+#         "STDERR": []
+#     }
+#     s = subprocess.run('git status -vv --long', stdout=subprocess.PIPE,
+#                        stderr=subprocess.PIPE, shell=True)
+#     for i in s.stdout.decode('utf-8').splitlines():
+#         out['STDOUT'].append(str(i))
+#     for i in s.stderr.decode('utf-8').splitlines():
+#         out['STDERR'].append(str(i))
+#     return out
+
+
+# def git_reset():
+#     out = {
+#         "STDOUT": [],
+#         "STDERR": []
+#     }
+#     s = subprocess.run('git reset --hard', stdout=subprocess.PIPE,
+#                        stderr=subprocess.PIPE, shell=True)
+#     for i in s.stdout.decode('utf-8').splitlines():
+#         out['STDOUT'].append(str(i))
+#     for i in s.stderr.decode('utf-8').splitlines():
+#         out['STDERR'].append(str(i))
+#     return out
 
 
 def choose_output(Data):
@@ -154,11 +144,20 @@ def run():
     if (Hash != PayloadHash):
         exit_on_error(None, 'UNABLE TO VALIDATE PAYLOAD', 403)
 
-    DataStorage['Git']['Status']['Before'] = git_status()
+    out = {}
 
-    DataStorage['Git']['Status']['After'] = git_status()
+    if (DataStorage['Payload']['repository']['name'] in Settings['Repositories'] or DataStorage['Payload']['repository']['full_name'] in Settings['Repositories']):
+        pass
+    else:
+        exit_on_error('', "NO CONFIGURATION FOR REPOSITORY", 404)
 
-    choose_output(DataStorage)
+    # with cd()
+
+    # DataStorage['Git']['Status']['Before'] = git_status()
+
+    # DataStorage['Git']['Status']['After'] = git_status()
+
+    choose_output(out)
 
 
 run()
